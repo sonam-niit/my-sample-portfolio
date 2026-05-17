4d000c10c415fd42ba919a62e7ef43c3f255637f
abhi-singh8 <abhishekks921@gmail.com>
2026-05-17 23:34:49 +0530

    fourth changed

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ console.log("Hello, World! This is my sample portfolio.");
 
 // You can add more JavaScript code here to enhance your portfolio's functionality.
 // For example, you could add event listeners, animations, or fetch data from an API.   
+// Example: Adding a click event listener to a button
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button clicked! This is a sample alert.");
+});
