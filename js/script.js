725c2ae3026d165f8d4590f76d96c0148dfd2b96
abhi-singh8 <abhishekks921@gmail.com>
2026-05-17 23:32:23 +0530

    sencond change

---
+++ js/script.js
@@ -0,0 +1,4 @@
+console.log("Hello, World! This is my sample portfolio.");
+
+// You can add more JavaScript code here to enhance your portfolio's functionality.
+// For example, you could add event listeners, animations, or fetch data from an API.   
